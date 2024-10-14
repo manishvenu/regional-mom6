a582714e0962c13ae4bc4a83721749d9833de20d
manishvenu <manish.venumuddula@gmail.com>
2024-10-14 14:07:13 -0600

    Remvoe premade_run_dir_arg

--- regional_mom6/regional_mom6.py
+++ regional_mom6/regional_mom6.py
@@ -2174,8 +2174,7 @@ class experiment:
         using_payu=False,
         overwrite=False,
         with_tides=False,
-        boundaries=["south", "north", "west", "east"],
-        premade_rundir_path_arg=None,
+        boundaries=["south", "north", "west", "east"]
     ):
         """
         Set up the run directory for MOM6. Either copy a pre-made set of files, or modify
@@ -2193,14 +2192,11 @@ class experiment:
         """
 
         ## Get the path to the regional_mom package on this computer
-        if premade_rundir_path_arg is None:
-            premade_rundir_path = Path(
-                importlib.resources.files("regional_mom6")
-                / "demos"
-                / "premade_run_directories"
-            )
-        else:
-            premade_rundir_path = premade_rundir_path_arg
+        premade_rundir_path = Path(
+            importlib.resources.files("regional_mom6")
+            / "demos"
+            / "premade_run_directories"
+        )
         if not premade_rundir_path.exists():
             print("Could not find premade run directories at ", premade_rundir_path)
             print(
